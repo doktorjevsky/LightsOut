83ebaa3a4842d78a40a0103439854109c437fd1a
doktorjevsky <97901243+doktorjevsky@users.noreply.github.com>
2023-01-02 17:34:40 +0100

    more tests

--- src/test/java/modelTests/TestGridModel.java
+++ src/test/java/modelTests/TestGridModel.java
@@ -5,6 +5,8 @@ import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
+import java.util.List;
+
 
 public class TestGridModel {
 
@@ -25,5 +27,38 @@ public class TestGridModel {
     public void lightsAreNotOut(){
         grid2.toggleCell(0, 0);
         Assertions.assertFalse(grid2.lightsAreOut());
+    }
+
+    // Mutating the grid in the GridModel doesn't mutate previously requested grids
+    @Test
+    public void testNotMutable1(){
+        List<List<Integer>> before = grid2.getGrid();
+        grid2.toggleCell(0, 0);
+        List<List<Integer>> after = grid2.getGrid();
+
+        boolean result = true;
+        for (int i = 0; i < before.size(); i++){
+            List<Integer> b = before.get(i);
+            List<Integer> a = after.get(i);
+            result = result && a.equals(b);
+        }
+        Assertions.assertFalse(result);
+    }
+
+    // Mutating a requested grid doesn't mutate the grid in the GridModel
+    @Test
+    public void testNotMutable2(){
+        List<List<Integer>> before = grid2.getGrid();
+        before.get(0).set(0, 100);
+        List<List<Integer>> after = grid2.getGrid();
+
+        boolean result = true;
+        for (int i = 0; i < before.size(); i++){
+            List<Integer> b = before.get(i);
+            List<Integer> a = after.get(i);
+            result = result && a.equals(b);
+        }
+
+        Assertions.assertFalse(result);
     }
 }
