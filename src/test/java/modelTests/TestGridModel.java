01baba0e9ebc15406c3e9d9d3462718a34ce9cf8
doktorjevsky <97901243+doktorjevsky@users.noreply.github.com>
2023-01-02 13:20:42 +0100

    more tests

--- src/test/java/modelTests/TestGridModel.java
+++ src/test/java/modelTests/TestGridModel.java
@@ -1,4 +1,29 @@
 package modelTests;
 
+import model.GridModel;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+
 public class TestGridModel {
+
+
+    private GridModel grid2;
+
+    @BeforeEach
+    public void init(){
+        grid2 = new GridModel(2, GenTestGrid.genGrid(2));
+    }
+
+    @Test
+    public void lightsAreOut(){
+        Assertions.assertTrue(grid2.lightsAreOut());
+    }
+
+    @Test
+    public void lightsAreNotOut(){
+        grid2.toggleCell(0, 0);
+        Assertions.assertFalse(grid2.lightsAreOut());
+    }
 }
