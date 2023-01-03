0100c55471c4bda19f43b779ecdccd76cefe1c5b
doktorjevsky <97901243+doktorjevsky@users.noreply.github.com>
2023-01-03 09:58:39 +0100

    added javafx

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,7 +1,25 @@
-public class Main {
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+public class Main extends Application {
 
 
     public static void main(String[] args){
-        System.out.println("Application is running");
+        launch(args);
+    }
+
+
+    @Override
+    public void start(Stage stage) throws Exception {
+
+        FXMLLoader loader = new FXMLLoader(getClass().getResource("fxml/mainMenuView.fxml"));
+        Parent root = loader.load();
+        stage.setTitle("Lights Out");
+        Scene scene = new Scene(root);
+        stage.setScene(scene);
+        stage.show();
     }
 }
---
+++ src/main/java/controllers/MainMenuController.java
@@ -0,0 +1,18 @@
+package controllers;
+
+import javafx.event.ActionEvent;
+
+public class MainMenuController {
+
+
+
+
+    public void exitButtonHandler(ActionEvent actionEvent) {
+    }
+
+    public void howToPlayButtonHandler(ActionEvent actionEvent) {
+    }
+
+    public void playButtonHandler(ActionEvent actionEvent) {
+    }
+}
